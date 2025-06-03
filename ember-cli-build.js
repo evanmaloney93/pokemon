6c0eb7f307c09d3d09a89f114a0b57e26c30f0be
evanmaloney93 <maloneyevan2@gmail.com>
2025-06-03 21:05:36 +0100

    build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,13 +4,16 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    rootURL: '/pokemon/',
+
     emberData: {
       deprecations: {
         DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
       },
     },
+
     fingerprint: {
-      prepend: 'https://evanmaloney93.github.io/pokemon/'
+      prepend: 'https://evanmaloney93.github.io/pokemon/',
     }
   });
 
