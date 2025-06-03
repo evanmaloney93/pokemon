14c5646039e2d5c79d03000f8c8b7b78fe82abed
evanmaloney93 <maloneyevan2@gmail.com>
2025-06-03 21:00:23 +0100

    Add fingerprint prepend for GitHub Pages

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,14 +6,12 @@ module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     emberData: {
       deprecations: {
-        // New projects can safely leave this deprecation disabled.
-        // If upgrading, to opt-into the deprecated behavior, set this to true and then follow:
-        // https://deprecations.emberjs.com/id/ember-data-deprecate-store-extends-ember-object
-        // before upgrading to Ember Data 6.0
         DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
       },
     },
-    // Add options here
+    fingerprint: {
+      prepend: 'https://evanmaloney93.github.io/pokemon/'
+    }
   });
 
   return app.toTree();
